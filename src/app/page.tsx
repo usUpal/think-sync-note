6b134e24c2492029a734d0022cc99077cb5c2c84
usupal <udyan.upal@gmail.com>
2023-09-28 00:06:18 +0600

    📝 chore: add .eslintrc.json configuration file to extend next/core-web-vitals for linting 📝 chore: add .gitignore file to ignore commonly ignored files and directories 📝 chore: add README.md file with project information and instructions 📝 chore: add components.json file with configuration for UI components 📝 chore: add next.config.js file for Next.js configuration 📝 chore: add package.json file with project dependencies
    📝 chore: add Tailwind CSS configuration file
    
    📝 chore: add TypeScript configuration file
    
    📝 chore: add global CSS file for Tailwind CSS styles
    
    📝 chore: add layout component
    
    📝 chore: add page component
    
    📝 chore: add sign-up page component
    
    📝 chore: add button component
    
    📝 chore: add utility function for class names
    
    📝 chore: add middleware for authentication
    
    📝

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className='text-red-600'>hello world</h1>
+      <Button>Add</Button>
+    </div>
+  );
+}
